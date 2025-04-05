9d276b3d82be6bc19f27e8c459e90d3516ac617f
Sreejesh06 <sreejesh25122006@gmail.com>
2025-04-05 09:47:48 +0530

    refactor:Update App component with new assets and improve button structure

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import gptLogo from "./assets/chatgpt.svg";
-import addbtn from "./assets/addbtn.svg";
+import addbtn from "./assets/add-30.png";
+import msgicon from "./assets/message.svg";
+
 function App() {
   return (
     <div className="App">
@@ -10,34 +12,39 @@ function App() {
             <img src={gptLogo} alt="logo" className="logo" />
             <span className="brand">ChatGPT</span>
           </div>
-          <button className="midBtn"><img src={} alt="" className="addbtn" />NewChat</button>
+          <button className="midBtn">
+            <img src={addbtn} alt="" className="addbtn" />
+            NewChat
+          </button>
+
           <div className="upperSideBottom">
-            <button className="query">What is Programming</button>
             <button className="query">
-              <img src="" alt="" />
+              <img src={msgicon} alt="" />
+              What is Programming
+            </button>
+            <button className="query">
+              <img src={msgicon} alt="" />
               What is Programming
             </button>
           </div>
         </div>
 
         <div className="lowerSide">
-
           <div className="listItems">
-            <img src={} alt="" className="listItemsImg" />
+            <img src={""} alt="" className="listItemsImg" />
             Home
           </div>
           <div className="listItems">
-            <img src={} alt="" className="listItemsImg" />
-Saved          </div>
+            <img src={""} alt="" className="listItemsImg" />
+            Saved{" "}
+          </div>
           <div className="listItems">
-            <img src={} alt="" className="listItemsImg" />
+            <img src={""} alt="" className="listItemsImg" />
             Upgrade
           </div>
         </div>
       </div>
-      <div className="main">
-
-      </div>
+      <div className="main"></div>
     </div>
   );
 }
